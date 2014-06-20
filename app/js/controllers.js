b545e9ad85b600521cd03e8f73ec3695e259cb40
shawn sandy <shawnsandy04@gmail.com>
2014-06-19 23:23:27 -0400

    Color Swatches sample

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,16 +13,15 @@ var sgApp = angular.module('sgApp.controllers', []);
         $scope.post = "views/post/preview.html";
         $scope.footer = "views/footer.html";
         $scope.comment = "views/footer.html";
-        $scope.colors = ['aqua','blue','navy','teal','green','olive','lime','yellow','orange','red','fuchsia','purple','maroon','white', 'sliver','gray','darkgray','black'];
-        $scope.sample_titles = ['A Sample Article','Another Sample Article', 'One More Sample title', 'Sample Article Number Four', 'One More For the Sake of it'];
-        $scope.e = {
-            'social': "views/elements/social.html",
+        $scope.colors = ['aqua','blue','navy','teal','olive','lime','yellow','orange','red','fuchsia','purple','maroon','white', 'sliver','gray','darkgray','black'];
+      $scope.sample_titles = ['A Sample Article','Another Sample Article', 'One More Sample title', 'Sample Article Number Four', 'One More For the Sake of it'];
+      $scope.e = {
+          'social': "views/elements/social.html",
             'author': "views/elements/author.html",
             'swatch': "views/elements/color-swatch.html",
             'subscribe': "views/elements/subscribe.html"
         };
         $scope.c = {
-
          'footer_copy' : 'views/footer/social_copy.html',
          'post_preview' : 'views/post/preview.html',
          'post_meta' : 'views/post/post-meta.html',
